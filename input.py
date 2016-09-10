1fa667bef7b784815cdef6335bff0ba557a7598d
sudhaprabhu <tanvi2993@gmail.com>
2016-09-10 09:30:54 +0530

    Addin modified input.py

--- input.py
+++ input.py
@@ -1,42 +1,89 @@
 import os
 import re
-import stat
-import sys
-#import collections
+import sys,getopt,time
 
-process=raw_input("Enter a Process name or PID:")
-os.system('sudo python parse.py '+process)
 
-f_out=open('a.out','r')
-if(os.path.getsize('a.out') <=0):
-    print "Please enter a valid Process name or PID"
+s_time='5'
+process=''
+
+#take argument to the program
+try:
+    myopts, args = getopt.getopt(sys.argv[1:],"p:t:")
+except getopt.GetoptError as e:
+    print (str(e))
+    print "Usage: python %s -p Process_Name/PID -t sleeptime" % sys.argv[0]
+    sys.exit()
+ 
+for o, a in myopts:
+    if o == '-p':
+        process=a
+    elif o == '-t':
+        s_time=a
+    
+if process=='':
+    print "Please provide Process Name/PID"
+    print "Usage: python %s -p Process_Name/PID -t sleeptime" % sys.argv[0]
     sys.exit()
 
-f=open('b.out','w')
-os.chmod('b.out',stat.S_IRWXU|stat.S_IRWXG|stat.S_IRWXO)
-for line in file('a.out'):
-     match = re.search(r'[0-9]+\.[0-9]+:.+', line)
-     f.write(match.group(0)+'\n')
-f.close()
+if not s_time.isdigit():
+    print "Please enter valid time"
+    print "Usage: python %s -p Process_Name/PID -t sleeptime" % sys.argv[0]
+    sys.exit()
 
+########################################################################################
+
+
+f_out=open('/tmp/a.out','w')
+#invoke parse.py 
+os.system('sudo python parse.py '+process+' '+s_time)
+
+f_out=open('/tmp/a.out','r')
+if(os.path.getsize('/tmp/a.out') <=0):
+    print "Please enter a valid Process name or PID"
+    os.remove('/tmp/a.out')
+    sys.exit()
+
+
+###########################################################################################
+
+#store data in dictionary
 dict={}
 
-for line in file('b.out'):
-    l=line.split()
+for line in file('/tmp/a.out'):
+    match = re.search(r'[0-9]+\.[0-9]+:.+', line)
+    ln=match.group(0).strip()
+    l=ln.split()
     t=l[0]
     o=l[5]
     d=l[6]
 
     dict[float(t[:len(t)-1])]=[int(o.split('=')[1]),int(d.split('=')[1])]
 
-#di=sorted(dict.items(),key=lambda t:t[0])
+#delete /tmp/a.out
+os.remove('/tmp/a.out')
+
+###########################################################################################
 
 if process.isdigit():
     pid=int(process)
     print "\nMigration of task %d\n" %pid
 else:
-    print "\nMigration of task %s\n" %process
+    print "\nMigration of task %s for time %fsec\n" %(process,float(s_time))
 
-for key in sorted(dict):
+
+t=sorted(dict.keys())
+
+#calucalte time delay 
+delta=map(lambda x,y:y-x,t[:len(t)-1],t[1:])
+delta.insert(0,0)
+
+
+for i,key in zip(delta,sorted(dict)):
+    time.sleep(i)
     print "At time %f task migrates from cpu %d to cpu %d" %(key,dict[key][0],dict[key][1])
+
+
+
+#############################################################################################
+
 
